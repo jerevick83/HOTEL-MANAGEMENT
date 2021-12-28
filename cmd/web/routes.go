7c25a8480d543753b4bd63b3d302bb07685066ca
jerevick83 <57697065+jerevick83@users.noreply.github.com>
2021-12-28 14:59:46 -0800

    Just another one

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,7 +5,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jerevick83/HOTEL-MGT/pkg/config"
 	"github.com/jerevick83/HOTEL-MGT/pkg/handlers"
-
 	"net/http"
 )
 
@@ -16,5 +15,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
